fe734fd1451539bccb90cce4ca013ffb7b80e2a1
Dawson Conway <dconman02@gmail.com>
2020-01-29 18:12:04 -0600

    fixed routes and started user

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resource :users
-  resource :merchants
-  resource :data_values
+  resources :users
+  resources :merchants
+  resources :data_values
 end
